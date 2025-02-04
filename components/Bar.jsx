7a06dcbcf5a7747f0bfc84c5d7f5cce75c90d212
Chitraksh09error <147639202+Chitraksh09error@users.noreply.github.com>
2025-02-04 12:32:51 +0530

    linkadded

--- components/Bar.jsx
+++ components/Bar.jsx
@@ -6,18 +6,25 @@ import { ChevronDown } from 'lucide-react';
 const Bar = () => {
   return (
     <div className="flex px-20 flex-col items-center justify-center py-4 mt-8">
-      <button className='bg-black  rounded-lg lg:px-8 lg:py-2 px-6 py-1 hover:bg-gray-400 '>
+      {/* <button className='bg-black  rounded-lg lg:px-8 lg:py-2 px-6 py-1 hover:bg-gray-400 '>
 
       <Link href="/login">
       <h1 className='lg:text-lg text-base text-white font-bold hover:text-black'>LOGIN</h1>
       
       </Link>
 
-      </button>
+      </button> */}
       <div className="w-full mt-3 h-1 bg-black"></div>
 
-      <p className="text-gray-700 mt-2 flex font-bold items-center gap-2">
-        Login to scroll down
+      <p className="text-gray-700 mt-2  flex font-bold items-center gap-2">
+        <Link href="/login" className="text-blue-400 underline hover:text-xl transition ease-in-out duration-500">
+          Login
+        </Link>
+
+
+        <h1>
+          to scroll down
+        </h1>
         <ChevronDown className="w-5 h-5 animate-bounce" />
       </p>
     </div>
